33315205e9795609064c8eeeb2ae17c2f00bc5d0
smartshark27 <thomassmart97@gmail.com>
2019-11-24 22:18:01 +1100

    Init graph with data

--- website/src/App.js
+++ website/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import HomeButton from "./components/HomeButton";
-const cytoscape = require("./Cytoscape");
+import D3Graph from "./components/Graph";
 
-const technologyNodes = require("./data/technology.json").nodes;
-const projectNodes = require("./data/project.json").nodes;
-const technologyTechnologyEdges = require("./data/technology-technology.json").edges;
-const projectTechnologyEdges = require("./data/project-technology.json").edges;
+const technologyNodes = require("./data/technology.json");
+const projectNodes = require("./data/project.json");
+const technologyTechnologyEdges = require("./data/technology-technology.json");
+const projectTechnologyEdges = require("./data/project-technology.json");
 const allNodes = technologyNodes.concat(projectNodes);
-const allEdges = technologyTechnologyEdges.concat(projectTechnologyEdges);
+const allLinks = technologyTechnologyEdges.concat(projectTechnologyEdges);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,47 +15,42 @@ export default class App extends React.Component {
     this.state = {
       windowWidth: 0,
       windowHeight: 0,
-      nodes: allNodes,
-      edges: allEdges,
+      data: {
+        nodes: allNodes,
+        links: allLinks
+      },
       selectedNode: null
     };
-    this.renderCytoscapeElement = this.renderCytoscapeElement.bind(this);
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    this.updateNodesAndEdges = this.updateNodesAndEdges.bind(this);
-    this.handleNodeClick = this.handleNodeClick.bind(this);
-    this.handleHomeButtonClick = this.handleHomeButtonClick.bind(this);
+    // this.updateNodesAndEdges = this.updateNodesAndEdges.bind(this);
+    // this.handleNodeClick = this.handleNodeClick.bind(this);
+    // this.handleHomeButtonClick = this.handleHomeButtonClick.bind(this);
   }
 
-  renderCytoscapeElement() {
-    this.cy = cytoscape.getCytoscapeElement(this.state.nodes, this.state.edges);
-    this.cy.nodes().on('click', this.handleNodeClick)
-  }
+  // handleNodeClick(e) {
+  //   const selectedNode = e.target;
+  //   const edges = allEdges.filter(edge => {
+  //     return edge.data.source === e.target.data('id') || edge.data.target === e.target.data('id')
+  //   })
+  //   const nodes = allNodes.filter(node => {
+  //     return node.data.id === e.target.data('id') || edges.reduce((isIn, curr) => {
+  //       return isIn || node.data.id === curr.data.source || node.data.id === curr.data.target;
+  //     }, false);
+  //   })
+  //   this.updateNodesAndEdges(selectedNode, nodes, edges);
+  // }
 
-  handleNodeClick(e) {
-    const selectedNode = e.target;
-    const edges = allEdges.filter(edge => {
-      return edge.data.source === e.target.data('id') || edge.data.target === e.target.data('id')
-    })
-    const nodes = allNodes.filter(node => {
-      return node.data.id === e.target.data('id') || edges.reduce((isIn, curr) => {
-        return isIn || node.data.id === curr.data.source || node.data.id === curr.data.target;
-      }, false);
-    })
-    this.updateNodesAndEdges(selectedNode, nodes, edges);
-  }
+  // handleHomeButtonClick() {
+  //   this.updateNodesAndEdges(null, allNodes, allEdges);
+  // }
 
-  handleHomeButtonClick() {
-    this.updateNodesAndEdges(null, allNodes, allEdges);
-  }
-
-  updateNodesAndEdges(selectedNode, nodes, edges) {
-    const state = this.state;
-    state.selectedNode = selectedNode;
-    state.nodes = nodes;
-    state.edges = edges;
-    this.setState(state);
-    this.renderCytoscapeElement();
-  }
+  // updateNodesAndEdges(selectedNode, nodes, edges) {
+  //   const state = this.state;
+  //   state.selectedNode = selectedNode;
+  //   state.nodes = nodes;
+  //   state.edges = edges;
+  //   this.setState(state);
+  // }
 
   updateWindowDimensions() {
     const state = this.state;
@@ -67,7 +62,6 @@ export default class App extends React.Component {
   componentDidMount() {
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
-    this.renderCytoscapeElement();
   }
 
   componentWillUnmount() {
@@ -77,8 +71,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <HomeButton handleClick={this.handleHomeButtonClick}/>
-        <div style={{ backgroundColor: "black", width: this.state.windowWidth, height: this.state.windowHeight }} id="cy" />
+        <HomeButton handleClick={this.handleHomeButtonClick} />
+        <D3Graph data={this.state.data} width={this.state.windowWidth} height={this.state.windowHeight} />
       </div>
     );
   }
