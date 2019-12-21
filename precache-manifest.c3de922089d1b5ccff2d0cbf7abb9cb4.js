f0364a01ca12a194e3804cf3d877a87b2d23ec6a
smartshark27 <thomassmart97@gmail.com>
2019-12-21 12:57:21 +1100

    Updates

---
+++ precache-manifest.c3de922089d1b5ccff2d0cbf7abb9cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bfc5b58316e5eb7fbc7f02e923c77e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56afda07e3a77ef9478",
+    "url": "/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "1614c6d5b12afe773305",
+    "url": "/static/js/2.518a3bb8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.518a3bb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c56afda07e3a77ef9478",
+    "url": "/static/js/main.4450232c.chunk.js"
+  },
+  {
+    "revision": "92bf4db728d85e70dbc5",
+    "url": "/static/js/runtime-main.ed7e7bd4.js"
+  }
+]);
