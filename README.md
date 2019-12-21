66b650ee342c9013060dc92e02c808e73881133b
smartshark27 <thomassmart97@gmail.com>
2019-12-21 12:38:23 +1100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# interactive-resume
-A website showcasing all the technologies used, projects worked on, and how they're related.
---
+++ precache-manifest.bb7c02ed7e49c8f98fcefc7b438a2557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d586b8766ce5e9ac127f80a1308222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9290a43c87dab0c99ee5",
+    "url": "/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "5c322eb2d0b6f5adc60f",
+    "url": "/static/js/2.808e6264.chunk.js"
+  },
+  {
+    "revision": "9290a43c87dab0c99ee5",
+    "url": "/static/js/main.3573bb4e.chunk.js"
+  },
+  {
+    "revision": "911285667be9a72280fa",
+    "url": "/static/js/runtime-main.ce5548e2.js"
+  }
+]);
