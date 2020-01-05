96c7a73e70af829f7d117e08047f44a3c63933ba
smartshark27 <thomassmart97@gmail.com>
2020-01-05 22:07:33 +1100

    Add other properties to info overlay

---
+++ build/precache-manifest.ee9ca31d9794c17412d04751ab62768a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a316470a4adb9f2311963ad588d2a9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a90000498f993f9d0dc",
+    "url": "/static/css/main.87b105c1.chunk.css"
+  },
+  {
+    "revision": "678e2f6377220bdae329",
+    "url": "/static/js/2.c2c86e9f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.c2c86e9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a90000498f993f9d0dc",
+    "url": "/static/js/main.5fe8e826.chunk.js"
+  },
+  {
+    "revision": "92bf4db728d85e70dbc5",
+    "url": "/static/js/runtime-main.ed7e7bd4.js"
+  }
+]);
--- src/components/InfoOverlay.js
+++ src/components/InfoOverlay.js
@@ -1,44 +1,123 @@
 import React from "react";
-import { Container } from '@material-ui/core';
+import { Box, Container } from '@material-ui/core';
 const appearance = require("../appearance.json");
 
 const style = {
     position: "absolute",
-    zIndex: 101, // Render above everything else
+    zIndex: 101,
+    minHeight: 1,
     borderRadius: 16,
     paddingBottom: 10,
 }
 
 export default function InfoOverlay(props) {
-    const sn = props.selectedNode
-    if (sn === null) {
-        return null
+    if (noNodeSelected(props.selectedNode)) {
+        return null;
     }
-
-    const isNarrowScreen = props.windowWidth <= 400;
-    const dynamicStyle = {
-        backgroundColor: appearance[sn.type].color,
-        minHeight: 1,
-    }
-    if (isNarrowScreen) {
-        dynamicStyle["maxWidth"] = props.windowWidth - 20
-        dynamicStyle["minHeight"] = 1
-        dynamicStyle["top"] = props.windowHeight - 75
-        dynamicStyle["left"] = 10
-        dynamicStyle["marginBottom"] = 10
-    } else {
-        dynamicStyle["maxWidth"] = props.windowWidth / 4
-        dynamicStyle["minHeight"] = 1
-        dynamicStyle["maxHeight"] = props.windowHeight - 40
-        dynamicStyle["top"] = 20
-        dynamicStyle["right"] = 20
-        dynamicStyle["overflowY"] = "scroll"
-    }
+    const dynamicStyle = createDynamicStyle(props.selectedNode, props.windowWidth, props.windowHeight);
 
     return (
-        <Container style={{ ...style, ...dynamicStyle }}>
-            <h2>{sn.name}</h2>
-            <p>{sn.description}</p>
+        <Container style={dynamicStyle}>
+            <CommonProperties selectedNode={props.selectedNode}/>
+            <ProjectPropertiesDiv selectedNode={props.selectedNode}/>
+            <TechnologyProperties selectedNode={props.selectedNode}/>
         </Container>
     )
+}
+
+function noNodeSelected(selectedNode) {
+    return selectedNode === null;
+}
+
+function createDynamicStyle(selectedNode, windowWidth, windowHeight) {
+    const dynamicStyle = {
+        ...style,
+        backgroundColor: appearance[selectedNode.type].color,
+    }
+    if (isNarrowScreen(windowWidth)) {
+        dynamicStyle["maxWidth"] = windowWidth - 20;
+        dynamicStyle["top"] = windowHeight - 75;
+        dynamicStyle["left"] = 10;
+        dynamicStyle["marginBottom"] = 10;
+    } else {
+        dynamicStyle["maxWidth"] = windowWidth / 4;
+        dynamicStyle["maxHeight"] = windowHeight - 40;
+        dynamicStyle["top"] = 20;
+        dynamicStyle["right"] = 20;
+        dynamicStyle["overflowY"] = "scroll";
+    }
+    return dynamicStyle;
+}
+
+function isNarrowScreen(windowWidth) {
+    return windowWidth <= 400;
+}
+
+function CommonProperties(props) {
+    return (
+        <>
+            <h6>{props.selectedNode.type.toUpperCase()}</h6>
+            <h2>{props.selectedNode.name}</h2>
+            <p>{props.selectedNode.description}</p>
+        </>
+    )
+}
+
+function ProjectPropertiesDiv(props) {
+    const sn = props.selectedNode;
+    const grade = (sn.grade) ? sn.grade.toString() + "/100" : null; 
+    return (
+        <>
+            <OneLineProperty displayName="Year" property={sn.year}/>
+            <OneLineProperty displayName="Grade" property={grade}/>
+            <Link displayName="Source Code" property={sn.code}/>
+        </>
+    )
+}
+
+function TechnologyProperties(props) {
+    const sn = props.selectedNode;
+    return (
+            <OneLineProperty displayName="Skill Level" property={skillLevelMap(sn.skillLevel)}/>
+    )
+}
+
+function skillLevelMap(n) {
+    if (n === 0) {
+        return "Haven't used";
+    } else if (n === 1) {
+        return "Some knowledge";
+    } else if (n === 2) {
+        return "Moderate experience";
+    } else if (n === 3) {
+        return "Deep understanding"
+    } else {
+        return "";
+    }
+}
+
+function OneLineProperty(props) {
+    const displayName = props.displayName;
+    const property = props.property;
+    if (!property) {
+        return null;
+    } else {
+        return <p>{displayName + ": " + property}</p>
+    }
+}
+
+function Link(props) {
+    const displayName = props.displayName;
+    const property = props.property;
+    if (!property) {
+        return null;
+    } else {
+        return (
+            <>
+                <Box>{displayName + ":"}</Box>
+                <a href={property}>{property}</a>
+                <p/>
+            </>
+        )
+    }
 }
